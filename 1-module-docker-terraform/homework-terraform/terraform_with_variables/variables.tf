d33b5e0279e21cfb567af7f57220380e7a3fe998
Anna <prussana@gmail.com>
2026-01-18 19:56:47 +0000

    change to a terraform file

--- 1-module-docker-terraform/homework-terraform/terraform_with_variables/variables.tf
+++ 1-module-docker-terraform/homework-terraform/terraform_with_variables/variables.tf
@@ -1,19 +1,17 @@
 variable "credentials" {
   description = "My Credentials"
-  default     = "<Path to your Service Account json file>"
+  default     = "/user/path"
   #ex: if you have a directory where this file is called keys with your service account json file
   #saved there as my-creds.json you could use default = "./keys/my-creds.json"
 }
 
-
 variable "project" {
   description = "Project"
-  default     = "<Your Project ID>"
+  default     = "112233"
 }
 
 variable "region" {
   description = "Region"
-  #Update the below to your desired region
   default     = "us-central1"
 }
 
